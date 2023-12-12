3646e24baab2b4cdcedc2fb5028d56b4fee770c9
dcfash <samuelayobami88@gmail.com>
2023-12-12 14:00:41 +0100

    making changes in main for conflicts

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ print("I just made change so as to merge with the feature-branch")
 print("I just made change so as to merge with the feature-branch")
 
 print("I am makin changes from others ")
+
+print("another changes from main")
+
